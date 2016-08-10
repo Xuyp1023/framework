ded22c74132315963763e4b41341eef0e547728a
Hewei <hewei@bytter.com>
2016-08-10 14:24:11 +0800

    1.同步BTAssert，抛出BetterTradeException 2.同步freemark模板

--- src/main/java/com/betterjr/common/utils/BTAssert.java
+++ src/main/java/com/betterjr/common/utils/BTAssert.java
@@ -2,12 +2,17 @@ package com.betterjr.common.utils;
 
 import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
+import java.util.Collection;
+import java.util.Map;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.util.Assert;
+import org.springframework.util.CollectionUtils;
+import org.springframework.util.ObjectUtils;
+import org.springframework.util.StringUtils;
 
 import com.betterjr.common.exception.BytterException;
+import com.betterjr.common.exception.BytterTradeException;
 import com.betterjr.common.exception.BytterValidException;
 
 /**
@@ -15,7 +20,7 @@ import com.betterjr.common.exception.BytterValidException;
  * @author zhoucy
  *
  */
-public class BTAssert extends Assert {
+public class BTAssert   {
     private static final Logger logger = LoggerFactory.getLogger(BTAssert.class);
     private static int ASSERT_ERROR_CODE = 90001;
 
@@ -29,7 +34,18 @@ public class BTAssert extends Assert {
 
         isNotSmallValue(anValue, null, null, null);
     }
-
+    
+    public static void isTrue(boolean expression) {
+        
+        isTrue(expression, "业务异常 - 业务处理必须正常");
+    }
+    
+    public static void isTrue(boolean expression, String message) {
+        if (!expression) {
+            
+            createBytterExcepObject(BytterTradeException.class, message);
+        }
+    }
     /**
      * 判断数字是否太小，包括空值;如果太小，则抛出异常
      * 
@@ -181,6 +197,142 @@ public class BTAssert extends Assert {
                 }
             }
             createBytterExcepObject(anEx, anMessage);
+        }
+    }
+    
+    public static void isNull(Object object) {
+        
+        isNull(object, "业务参数必须为空");
+    }
+    
+    public static void isNull(Object object, String message) {
+        if (object != null) {
+            
+            createBytterExcepObject(BytterTradeException.class, message); 
+        }
+    }
+
+    public static void notNull(Object object, String message) {
+        if (object == null) {
+            
+            createBytterExcepObject(BytterTradeException.class, message); 
+        }
+    }
+    
+    public static void notNull(Object object) {
+        
+        notNull(object, "业务参数不能为空");
+    }
+    
+    public static void hasLength(String text, String message) {
+        if (StringUtils.hasLength(text) == false ) {
+            
+            createBytterExcepObject(BytterTradeException.class, message); 
+        }
+    }
+    
+    public static void hasLength(String text) {
+        
+        hasLength(text, "字符长度不够，不能为空或者空格");
+    }
+    
+    public static void hasText(String text, String message) {
+        if (!StringUtils.hasText(text)) {
+            
+            createBytterExcepObject(BytterTradeException.class, message); 
+        }
+    }
+    
+    public static void hasText(String text) {
+        
+        hasText(text, "字符必須要有內容，不能为空或者空格");
+    }
+
+    public static void doesNotContain(String textToSearch, String substring, String message) {
+        if (StringUtils.hasLength(textToSearch) && StringUtils.hasLength(substring) &&
+                textToSearch.contains(substring)) {
+            createBytterExcepObject(BytterTradeException.class, message);
+        }
+    }
+    
+    public static void doesNotContain(String textToSearch, String substring) {
+        
+        doesNotContain(textToSearch, substring, "字符串 ["+textToSearch+"] 不能包含 字符串 [" + substring + "]");
+    }
+    
+    public static void notEmpty(Object[] array, String message) {
+        if (ObjectUtils.isEmpty(array)) {
+            createBytterExcepObject(BytterTradeException.class, message);
+        }
+    }
+    
+    public static void notEmpty(Object[] array) {
+        
+        notEmpty(array, "数据数组不能为空: 必须至少包含一条数据");
+    }
+
+    public static void noNullElements(Object[] array, String message) {
+        if (array != null) {
+            for (Object element : array) {
+                if (element == null) {
+                    createBytterExcepObject(BytterTradeException.class, message);
+                }
+            }
+        }
+    }
+
+    public static void noNullElements(Object[] array) {
+        
+        noNullElements(array, "数组不能包括任何空的数据，不能有一条空的数据");
+    }
+    
+    public static void notEmpty(Collection<?> collection, String message) {
+        if (CollectionUtils.isEmpty(collection)) {
+            
+            createBytterExcepObject(BytterTradeException.class, message);
+        }
+    }
+
+    public static void notEmpty(Collection<?> collection) {
+        
+        notEmpty(collection, "数据列表不能为空，至少包括一条数据");
+    }
+    
+    public static void notEmpty(Map<?, ?> map, String message) {
+        if (CollectionUtils.isEmpty(map)) {
+            createBytterExcepObject(BytterTradeException.class, message);
+        }
+    }
+    
+    public static void notEmpty(Map<?, ?> map) {
+        
+        notEmpty(map, "数据映射集合不能为空，至少包括条数据");
+    }
+    
+    public static void isInstanceOf(Class<?> clazz, Object obj) {
+        
+        isInstanceOf(clazz, obj, "");
+    }    
+    
+    public static void isInstanceOf(Class<?> type, Object obj, String message) {
+        notNull(type, "检查JAVA对象类型为空");
+        if (!type.isInstance(obj)) {
+            throw new IllegalArgumentException(
+                    (StringUtils.hasLength(message) ? message + " " : "") +
+                    "对象的类 [" + (obj != null ? obj.getClass().getName() : "空") +
+                    "] 必须是 【" + type+"】的一个实例");
+        }
+    }
+    
+    public static void isAssignable(Class<?> superType, Class<?> subType) {
+        isAssignable(superType, subType, "");
+    }
+    
+    public static void isAssignable(Class<?> superType, Class<?> subType, String message) {
+        notNull(superType, "检查JAVA对象类型为空");
+        if (subType == null || !superType.isAssignableFrom(subType)) {
+            throw new IllegalArgumentException((StringUtils.hasLength(message) ? message + " " : "") +
+                    subType + " 不是继承类  " + superType);
         }
     }
     
