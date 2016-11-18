187957affbda9fb8bcfb57f79d26a01469130260
Hubl <hubl@bytter.com>
2016-11-18 17:19:54 +0800

    task-20161118-1   客户关系功能新增

--- src/main/java/com/betterjr/modules/account/dubbo/CustInfoDubboService.java
+++ src/main/java/com/betterjr/modules/account/dubbo/CustInfoDubboService.java
@@ -1,15 +1,19 @@
 package com.betterjr.modules.account.dubbo;
 
 import java.util.Collection;
+import java.util.List;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import com.alibaba.dubbo.config.annotation.Service;
+import com.betterjr.common.utils.Collections3;
 import com.betterjr.common.utils.UserUtils;
 import com.betterjr.common.web.AjaxObject;
 import com.betterjr.modules.account.dubbo.interfaces.ICustInfoService;
 import com.betterjr.modules.account.entity.CustInfo;
+import com.betterjr.modules.account.service.CustAndOperatorRelaService;
 
 /**
  * 机构基本信息
@@ -21,6 +25,9 @@ import com.betterjr.modules.account.entity.CustInfo;
 public class CustInfoDubboService implements ICustInfoService {
     private static Logger logger = LoggerFactory.getLogger(CustInfoDubboService.class);
 
+    @Autowired
+    private CustAndOperatorRelaService custOperatorRelaService;
+    
     @Override
     public String webQueryCustInfo() {
         Collection<CustInfo> custInfos = queryCustInfo();
@@ -30,6 +37,16 @@ public class CustInfoDubboService implements ICustInfoService {
     @Override
     public Collection<CustInfo> queryCustInfo() {
         return UserUtils.findCustInfoList();
+    }
+    
+    /***
+     * 获取当前登录的客户号
+     * @return
+     */
+    public Long findCustNo(){
+        List<Long> custList=custOperatorRelaService.findCustNoList(UserUtils.getOperatorInfo().getId(),UserUtils.getOperatorInfo().getOperOrg());
+        logger.info("findCustNo custList:"+custList);
+        return Collections3.getFirst(custList);
     }
 
 }
