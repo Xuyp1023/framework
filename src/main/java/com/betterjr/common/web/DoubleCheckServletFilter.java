56b4194a40b0d42f8a1767fbec1249a06dde8dd6
zhoucy <zhoucy@bytter.com>
2017-01-06 11:53:02 +0800

    重复提交拦截处理

---
+++ src/main/java/com/betterjr/common/web/DoubleCheckServletFilter.java
@@ -0,0 +1,49 @@
+package com.betterjr.common.web;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+
+import com.betterjr.common.utils.BetterStringUtils;
+import com.betterjr.common.utils.JedisUtils;
+
+public class DoubleCheckServletFilter implements Filter {
+
+    private static final String WEB_AJAX_TOKEN_KEY = "webAjaxToken::AccessToken::";
+
+    @Override
+    public void init(FilterConfig anFilterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest anRequest, ServletResponse anResponse, FilterChain anChain) throws IOException, ServletException {
+        String tmpAjaxToken = anRequest.getParameter("AjaxToken");
+        HttpServletRequest request = (HttpServletRequest)anRequest; 
+        
+        if (BetterStringUtils.isNotBlank(tmpAjaxToken)) {
+            String tmpValue = JedisUtils.get(WEB_AJAX_TOKEN_KEY.concat(tmpAjaxToken));
+            if (BetterStringUtils.isNotBlank(tmpValue)) {
+                anResponse.setContentType("application/json");
+                anResponse.getWriter().write(new AjaxObject(204, "请求太频繁").toJson());
+                return;
+            }
+            else {
+                JedisUtils.set(WEB_AJAX_TOKEN_KEY.concat(tmpAjaxToken), tmpAjaxToken, 30);
+            }
+        }
+        anChain.doFilter(anRequest, anResponse);
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+
+}
