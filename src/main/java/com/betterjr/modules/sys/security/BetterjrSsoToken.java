db2525d6ce0898cb6c51359fa548dfbf365abcff
liuwl <liuwl@bytter.com>
2017-03-26 15:55:38 +0800

    task-177

--- src/main/java/com/betterjr/modules/sys/security/BetterjrSsoToken.java
+++ src/main/java/com/betterjr/modules/sys/security/BetterjrSsoToken.java
@@ -8,6 +8,8 @@ public class BetterjrSsoToken extends UsernamePasswordToken {
 
     private final String ipAddress;
     private final String ticket;
+    private final String custRole;
+    private final String corpId;
 
     public String getTicket() {
         return ticket;
@@ -17,14 +19,26 @@ public class BetterjrSsoToken extends UsernamePasswordToken {
         return ipAddress;
     }
 
+    public String getCustRole() {
+        return custRole;
+    }
+
+    public String getCorpId() {
+        return corpId;
+    }
+
     public BetterjrSsoToken() {
         this.ticket = "";
         this.ipAddress = "";
+        this.custRole = "";
+        this.corpId = "";
     }
 
-    public BetterjrSsoToken(final String anTicket, final String anUsername, final String anPassword, final String anHost) {
+    public BetterjrSsoToken(final String anTicket, final String anCorpId, final String anCustRole, final String anUsername, final String anPassword, final String anHost) {
         super(anUsername, anPassword, false, anHost);
+        this.corpId = anCorpId;
         this.ticket = anTicket;
         this.ipAddress = anHost;
+        this.custRole = anCustRole;
     }
 }
