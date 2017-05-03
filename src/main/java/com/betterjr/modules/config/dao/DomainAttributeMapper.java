9f4b476e5423bba63d1a4724abe3ec7ca4fd6e50
liuwl <liuwl@bytter.com>
2017-05-03 12:36:05 +0800

    task-193

---
+++ src/main/java/com/betterjr/modules/config/dao/DomainAttributeMapper.java
@@ -0,0 +1,9 @@
+package com.betterjr.modules.config.dao;
+
+import com.betterjr.common.annotation.BetterjrMapper;
+import com.betterjr.mapper.common.Mapper;
+import com.betterjr.modules.config.entity.DomainAttribute;
+
+@BetterjrMapper
+public interface DomainAttributeMapper extends Mapper<DomainAttribute> {
+}
